84bdcb9f55eba96310aaab9296daccc64e803a59
RyleeGM <rmattingly@ou.edu>
2020-04-25 06:53:09 -0500

    Cleaned up the main function.

--- source/main.cpp
+++ source/main.cpp
@@ -7,9 +7,8 @@
 //  Copyright © 2020 RyleeGM. All rights reserved.
 //
 
-#include <iostream>
-#include <fstream>
-#include <string>
+#include "Instruction.hpp"
+#include "FileOperations.hpp"
 
 using namespace std;
 
@@ -19,41 +18,36 @@ int main(){
 
     //MARK: Variables
     ifstream test;          //Test File.
-    bool header = true;     //Tells if header is present in file.
+    ifstream trace;         //Trace File.
+    ifstream instructions;  //File containing instuction data.
     string nextString;      //Next String to be compared against.
     
     
     //MARK: File Load and Setup
     //Load the test file.
     test.open("InputFiles/test.txt");
-
+    trace.open("InputFiles/trace.out");
+    instructions.open("InputFiles/InstructionLatencies.txt");
+    
     //Check if the file failed to open.
-    if(test.fail()){
+    if(test.fail() || trace.fail() || instructions.fail()){
         return 1;
     }
     
-    //Peek to see if it is a header line.
-    //Clear the line if it is the header.
-    while(header){
-        if(!(isdigit(test.peek()))){
-            test.ignore(numeric_limits<streamsize>::max(), '\n');
-        } else {
-            header = false;
-        }
-    }
+    remHeader(&test);
+    remHeader(&trace);
+    remHeader(&instructions);
     
-//    int counterVal = 1;
-//    int insNum;
-//
-//    while(!test.eof()){
-//        test >> insNum;
-//        if(insNum != counterVal){
-//            cerr << "Error " << counterVal << "\n";
-//        }
-//        cerr << "Yes! " << counterVal << "\n";
-//        counterVal++;
-//        test.ignore(numeric_limits<streamsize>::max(), '\n');
-//    }
+    Instruction ins1 =      Instruction(&trace);
+    
+    cout << ins1;
+    
+    
+    
+    //Close files for clean exit.
+    test.close();
+    trace.close();
+    instructions.close();
     
     return 0;
 }
