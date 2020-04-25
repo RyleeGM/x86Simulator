68ba00fd509804a0652b33c777e20e89f3b35911
RyleeGM <rmattingly@ou.edu>
2020-04-25 11:32:30 -0500

    Added SimState files that will manage the simulation.

---
+++ source/SimState.cpp
@@ -0,0 +1,9 @@
+//
+//  SimState.cpp
+//  x86Simulator
+//
+//  Created by Rylee Mattingly on 4/25/20.
+//  Copyright © 2020 RyleeGM. All rights reserved.
+//
+
+#include "SimState.hpp"
---
+++ source/SimState.hpp
@@ -0,0 +1,22 @@
+//
+//  SimState.hpp
+//  x86Simulator
+//
+//  Created by Rylee Mattingly on 4/25/20.
+//  Copyright © 2020 RyleeGM. All rights reserved.
+//
+
+#ifndef SimState_hpp
+#define SimState_hpp
+
+#include "Instruction.hpp"
+
+class SimState{
+  
+    
+    
+};
+
+
+
+#endif /* SimState_hpp */
