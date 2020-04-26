dd18927d889debd9e0668c0677b425f7acbbef62
RyleeGM <rmattingly@ou.edu>
2020-04-26 08:40:56 -0500

    Partial first iteration of the simulator managing class.

--- source/SimState.cpp
+++ source/SimState.cpp
@@ -7,3 +7,41 @@
 //
 
 #include "SimState.hpp"
+
+using namespace std;
+
+// MARK: - Constructors
+
+/**
+ Construct the latency vectors for the simulator and sets setting variables.
+ @param latFile     ifstream to file containing latencies.
+ */
+SimState::SimState(ifstream *latFile){
+    
+    //Instantiates the latency vectors to 1587.
+    latencyList = vector<int>(1587);
+    recipLatencyList = vector<int>(1587);
+    
+    //Set the 0th element of each list to zeros.
+    latencyList[0] = 0;
+    recipLatencyList[0] = 0;
+    
+    int indexChecker = 0;
+    
+    for(int i = 1; i < 1587; i++){
+        *latFile >> indexChecker;
+        
+        //Check that the index is correct.
+        if(indexChecker != i){
+            cerr << "File Index Error:" << i << indexChecker;
+            return;
+        }
+        
+        //Read in the values from the file and store them.
+        *latFile >> latencyList[i] >> recipLatencyList[i];
+        
+        //Go to next line.
+        latFile->ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    
+}
--- source/SimState.hpp
+++ source/SimState.hpp
@@ -9,10 +9,33 @@
 #ifndef SimState_hpp
 #define SimState_hpp
 
+#include <list>
 #include "Instruction.hpp"
 
 class SimState{
   
+private:
+    //Master Lists for latency and reciprical latency.
+    std::vector<int> latencyList;
+    std::vector<int> recipLatencyList;
+    //List of instructions in the repective sections of the simulator.
+    std::list<Instruction> fetchedIns;
+    std::list<Instruction> issuedIns;
+    //User set variables from settings file.
+    int issueWidth;
+    int fetchWidth;
+    int loadBlocks;
+    int storeBlocks;
+    int aluBlocks;
+    //Variables to track resource availability.
+    int availableStores;
+    int availableLoads;
+    int availableALU;
+    
+public:
+    //Constructors
+    SimState(std::ifstream *latFile);
+    
     
     
 };
