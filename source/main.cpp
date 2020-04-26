5efd8071202fe302f728f0b73cc8469c8c133539
RyleeGM <rmattingly@ou.edu>
2020-04-26 08:40:34 -0500

    Made variable names more clear.

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,7 @@
 //  Copyright © 2020 RyleeGM. All rights reserved.
 //
 
-#include "Instruction.hpp"
+#include "SimState.hpp"
 #include "FileOperations.hpp"
 
 using namespace std;
@@ -19,45 +19,37 @@ int main(){
     //MARK: Variables
     ifstream test;          //Test File.
     ifstream trace;         //Trace File.
-    ifstream instructions;  //File containing instuction data.
+    ifstream latencies;     //File containing instuction data.
     string nextString;      //Next String to be compared against.
     
     
     //MARK: File Load and Setup
-    //Load the test file.
+    //Load Files.
     test.open("InputFiles/test.txt");
     trace.open("InputFiles/trace.out");
-    instructions.open("InputFiles/InstructionLatencies.txt");
+    latencies.open("InputFiles/InstructionLatencies.txt");
     
-    //Check if the file failed to open.
-    if(test.fail() || trace.fail() || instructions.fail()){
+    //Check if any file failed to open.
+    if(test.fail() || trace.fail() || latencies.fail()){
+        cerr << "A File Failed to Open" << "\n" << "Exiting";
         return 1;
     }
     
+    //Remove file headers
     remHeader(&test);
     remHeader(&trace);
-    remHeader(&instructions);
+    remHeader(&latencies);
     
     //Temporary Testing Region.
     
-    Instruction ins [10];
-    
-    for(int i = 0; i < 10; i++){
-        ins[i] = Instruction(&trace);
-    }
-    
-    for(int i = 0; i < 10; i++){
-        cout << ins[i] << "\n";
-    }
-    
-    cout << ins[1].isProducer(vector<int>{1, 2, 7, 25}) << "\n";
+    SimState simulator = SimState(&latencies);
     
     //Temporary Testing Region end.
     
     //Close files for clean exit.
     test.close();
     trace.close();
-    instructions.close();
+    latencies.close();
     
     return 0;
 }
