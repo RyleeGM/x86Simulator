05b0f52799eb36ded2aaf9db4917b0f7bf7a9708
RyleeGM <rmattingly@ou.edu>
2020-04-30 09:24:59 -0500

    Added command line arguments for trace, latencies and settings.

--- source/main.cpp
+++ source/main.cpp
@@ -13,30 +13,33 @@ using namespace std;
 
 bool header = 1;
 
-int main(){
+int main(int argc, char *argv[]){
 
     //MARK: Variables
-    ifstream test;          //Test File.
     ifstream trace;         //Trace File.
     ifstream latencies;     //File containing instuction data.
     ifstream settings;      //File containing the settings.
     
+    string traceName = "InputFiles/";
+    traceName += argv[1];
+    string latencyName = "InputFiles/";
+    latencyName += argv[2];
+    string settingsName = "InputFiles/";
+    settingsName += argv[3];
     
     //MARK: File Load and Setup
     //Load Files.
-    test.open("InputFiles/test.txt");
-    trace.open("InputFiles/trace.out");
-    latencies.open("InputFiles/InstructionLatencies.txt");
-    settings.open("InputFiles/settings.txt");
+    trace.open(traceName);
+    latencies.open(latencyName);
+    settings.open(settingsName);
     
     //Check if any file failed to open.
-    if(test.fail() || trace.fail() || latencies.fail() || settings.fail()){
+    if(trace.fail() || latencies.fail() || settings.fail()){
         cerr << "A File Failed to Open" << "\n" << "Exiting";
         return 1;
     }
     
     //Remove file headers
-    remHeader(&test);
     remHeader(&trace);
     remHeader(&latencies);
     remHeader(&settings);
@@ -47,9 +50,9 @@ int main(){
     simulation.report();
     
     //Close files for clean exit.
-    test.close();
     trace.close();
     latencies.close();
+    settings.close();
     
     return 0;
 }
